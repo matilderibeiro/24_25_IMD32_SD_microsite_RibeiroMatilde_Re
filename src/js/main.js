0483e96f603d29522d4adc0d39dc9da462fd6069
matilderibeiro <122967605+matilderibeiro@users.noreply.github.com>
2024-11-13 15:14:41 +0100

    suite codage plus correction css js

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,94 @@
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 gsap.registerPlugin(ScrollTrigger);
 
-const titleheader = document.querySelector("header");
-const titleImg = document.querySelector(".header-intro");
-const butterfly = document.querySelector(".header-butterfly");
+const img1 = document.querySelector(".scroll-1");
+const img2 = document.querySelector(".scroll-2");
+const img3 = document.querySelector(".scroll-3");
+const img4 = document.querySelector(".scroll-4");
+const img5 = document.querySelector(".scroll-5");
+const img6 = document.querySelector(".scroll-6");
+const img7 = document.querySelector(".scroll-7");
+const img8 = document.querySelector(".scroll-8");
 
-var tl = gsap.timeline();
-tl.to(titleImg, { opacity: 1 });
-tl.to(titleImg, { duration: 3, opacity: 0 });
-tl.to(titleImg, { x: "100%", duration: 0.5, ease: "expo.out" });
-tl.fromTo(
-  butterfly,
-  { x: "-100%", ease: "expo.out", delay: 0 },
-  { x: "100", ease: "expo.out", delay: 0 }
-);
-tl.to(butterfly, { x: 200, y: -20 });
-tl.to(butterfly, { x: 300, y: -80 });
-tl.to(butterfly, { x: 500, y: -60, delay: 0 });
-tl.to(butterfly, { x: 600, y: 0, delay: 0 });
-tl.to(titleheader, { x: "100%", duration: 1, ease: "expo.out" });
+gsap.to(img1, {
+  scrollTrigger: {
+    trigger: img1,
+    start: "top 70%",
+    scrub: true,
+  },
+  y: 40,
+  opacity: 1,
+});
+
+gsap.to(img2, {
+  scrollTrigger: {
+    trigger: img2,
+    markers: true,
+    start: "top 90%",
+    scrub: true,
+  },
+  y: 40,
+  opacity: 1,
+});
+
+gsap.to(img3, {
+  scrollTrigger: {
+    trigger: img3,
+    start: "top 75%",
+    scrub: true,
+  },
+  y: 40,
+  opacity: 1,
+});
+
+gsap.to(img4, {
+  scrollTrigger: {
+    trigger: img4,
+    start: "top 80%",
+    scrub: true,
+  },
+  y: 40,
+  opacity: 1,
+});
+
+gsap.to(img5, {
+  scrollTrigger: {
+    trigger: img5,
+    start: "top 95%",
+    scrub: true,
+  },
+  y: 40,
+  opacity: 1,
+});
+
+gsap.to(img6, {
+  scrollTrigger: {
+    trigger: img6,
+    start: "top 95%",
+    scrub: true,
+  },
+  y: 40,
+  opacity: 1,
+});
+
+gsap.to(img7, {
+  scrollTrigger: {
+    trigger: img7,
+    start: "top 95%",
+    scrub: true,
+  },
+  y: 40,
+  opacity: 1,
+});
+
+gsap.to(img8, {
+  scrollTrigger: {
+    trigger: img8,
+    start: "top 70%",
+    scrub: true,
+  },
+  y: 40,
+  opacity: 1,
+});

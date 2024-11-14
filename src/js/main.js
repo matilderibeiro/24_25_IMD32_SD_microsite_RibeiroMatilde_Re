6e024bb99cdb4aa79ef23eb7286f0eec0c048a2b
matilderibeiro <122967605+matilderibeiro@users.noreply.github.com>
2024-11-14 09:57:13 +0100

    suite code plus animations header

--- src/js/main.js
+++ src/js/main.js
@@ -25,8 +25,7 @@ gsap.to(img1, {
 gsap.to(img2, {
   scrollTrigger: {
     trigger: img2,
-    markers: true,
-    start: "top 90%",
+    start: "top 70%",
     scrub: true,
   },
   y: 40,
@@ -36,7 +35,7 @@ gsap.to(img2, {
 gsap.to(img3, {
   scrollTrigger: {
     trigger: img3,
-    start: "top 75%",
+    start: "top 70%",
     scrub: true,
   },
   y: 40,
@@ -86,9 +85,67 @@ gsap.to(img7, {
 gsap.to(img8, {
   scrollTrigger: {
     trigger: img8,
-    start: "top 70%",
+    start: "top 60%",
     scrub: true,
   },
   y: 40,
   opacity: 1,
+});
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const titleheader = document.querySelector("header");
+const titleImg = document.querySelector(".header-title-intro");
+const butterfly = document.querySelector(".header-butterfly");
+
+var tl = gsap.timeline({
+  onComplete: () => (document.body.style.overflowY = "auto"),
+});
+tl.to(titleImg, { opacity: 1 });
+tl.to(titleImg, { duration: 3, opacity: 0 });
+tl.to(titleImg, { x: "100%", duration: 0.5, ease: "expo.out" });
+tl.fromTo(
+  butterfly,
+  { x: "-100%", ease: "expo.out", delay: 0 },
+  { x: "100", ease: "expo.out", delay: 0 }
+);
+tl.to(butterfly, { x: 200, y: -20 });
+tl.to(butterfly, { x: 300, y: -40 });
+tl.to(butterfly, { x: 450, y: -30 });
+tl.to(butterfly, { scale: 1.5 });
+tl.to(butterfly, { scale: 1 });
+tl.to(butterfly, { scale: 1.5 });
+tl.to(butterfly, { scale: 1 });
+tl.to(butterfly, { scale: 1.5 });
+tl.to(butterfly, { scale: 1 });
+tl.to(butterfly, { scale: 1.5 });
+tl.to(butterfly, { scale: 1 });
+tl.to(butterfly, { x: 500, y: -60, delay: 0 });
+tl.to(butterfly, { x: 600, y: 0, delay: 0 });
+tl.to(butterfly, { x: 900, y: -50, delay: 0 });
+tl.to(butterfly, { x: 1100, y: 40, delay: 0 });
+tl.to(butterfly, { x: 1500, y: -200, delay: 0 });
+tl.to(titleheader, { x: "100%", duration: 2, ease: "expo.out" });
+
+ScrollTrigger.addEventListener(
+  "refreshInit",
+  () => (document.body.style.overflowY = "auto")
+);
+ScrollTrigger.addEventListener("refresh", () => {
+  if (tl.progress() < 1) {
+    document.body.style.overflowY = "hidden";
+  }
+});
+
+const scrollHorizontal = document.querySelector(".pin-wrap");
+
+gsap.to(scrollHorizontal, {
+  scrollTrigger: {
+    trigger: scrollHorizontal,
+    start: "center center",
+    scrub: true,
+    pin: true,
+  },
+  x: 4400,
 });

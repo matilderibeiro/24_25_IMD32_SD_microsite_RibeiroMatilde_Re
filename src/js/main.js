b0a88d958954466648c12188860acd9dccc3480c
matilderibeiro <122967605+matilderibeiro@users.noreply.github.com>
2024-11-29 14:05:53 +0100

    suite code corrections animation train

--- src/js/main.js
+++ src/js/main.js
@@ -176,61 +176,37 @@ btnChoisePast.addEventListener("click", function () {
   gsap.to(window, { scrollTo: ".chose" });
 });
 
-// gsap.registerPlugin(ScrollTrigger);
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-// const pageContainer = document.querySelector(".container");
+  gsap.to(".scroll-container-view", {
+    scrollTrigger: {
+      trigger: ".intro",
+      start: "top top",
+      end: "+=600",
+      scrub: true,
+    },
+    x: "-500vw",
+  });
 
-// /* SMOOTH SCROLL */
-// const scroller = new LocomotiveScroll({
-//   el: pageContainer,
-//   smooth: true,
-// });
+  gsap.to(".scroll-train", {
+    scrollTrigger: {
+      trigger: ".intro",
+      start: "top top",
+      end: "+=600",
+      scrub: true,
+    },
+    x: "210vw",
+    duration: 3,
+  });
 
-// scroller.on("scroll", ScrollTrigger.update);
-
-// ScrollTrigger.scrollerProxy(pageContainer, {
-//   scrollTop(value) {
-//     return arguments.length
-//       ? scroller.scrollTo(value, 0, 0)
-//       : scroller.scroll.instance.scroll.y;
-//   },
-//   getBoundingClientRect() {
-//     return {
-//       left: 0,
-//       top: 0,
-//       width: window.innerWidth,
-//       height: window.innerHeight,
-//     };
-//   },
-//   pinType: pageContainer.style.transform ? "transform" : "fixed",
-// });
-
-// ////////////////////////////////////
-// ////////////////////////////////////
-// window.addEventListener("load", function () {
-//   let pinBoxes = document.querySelectorAll(".pin-wrap > *");
-//   let pinWrap = document.querySelector(".pin-wrap");
-//   let pinWrapWidth = pinWrap.offsetWidth;
-//   let horizontalScrollLength = pinWrapWidth - window.innerWidth;
-
-//   // Pinning and horizontal scrolling
-
-//   gsap.to(".pin-wrap", {
-//     scrollTrigger: {
-//       scroller: pageContainer, //locomotive-scroll
-//       scrub: true,
-//       trigger: "#sectionPin",
-//       pin: true,
-//       // anticipatePin: 1,
-//       start: "top top",
-//       end: pinWrapWidth,
-//     },
-//     x: -horizontalScrollLength,
-//     ease: "none",
-//   });
-
-//   ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
-
-//   ScrollTrigger.refresh();
-// });
-// script.js
+  gsap.to(".scroll-bloc-horizontal-butterfly", {
+    scrollTrigger: {
+      trigger: ".intro",
+      start: "top top",
+      end: "+=600",
+      scrub: true,
+    },
+    x: "100vw",
+  });
+});
